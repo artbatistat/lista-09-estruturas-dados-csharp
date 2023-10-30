0ebdc795a3f582e6e64d6b470939dbbee169e94f
Arthur Batista <52763653+artbatistat@users.noreply.github.com>
2023-10-29 23:15:42 -0300

    Add files via upload

---
+++ ex2.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+
+Hashtable CPF_Telefone = new Hashtable(); 
+        
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.Write("Digite o CPF da pessoa {0}: ",i);
+            string CPF = Console.ReadLine();
+
+            Console.Write("Digite o número de telefone da pessoa {0}: ",i);
+            string telefone = Console.ReadLine();
+
+            CPF_Telefone[CPF] = telefone;
+
+            Console.WriteLine();
+        }
+        foreach (DictionaryEntry entry in CPF_Telefone)
+        {
+            string cpf = (string)entry.Key;
+            string telefone = (string)entry.Value;
+
+            Console.WriteLine($"CPF: {cpf}, Telefone: {telefone}");
+        }
+
+        pesquisarCPF(CPF_Telefone);
+
+        Console.ReadLine();
+
+static void pesquisarCPF(Hashtable CPF_Telefone)
+    {
+        Console.WriteLine("Digite o CPF para pesquisar:");
+        string CPF = Console.ReadLine();
+        Console.WriteLine("CPF: {0}, Telefone: {1}",CPF,CPF_Telefone[CPF]);
+
+    }
